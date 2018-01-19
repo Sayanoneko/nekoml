fa9272447a92153aa058c7402b8f16bb59194338
Sayanoneko <benoit.le.niger@outlook.com>
2018-01-19 08:26:30 +0100

    first transformation functions created

--- nekoma.py
+++ nekoma.py
@@ -8,7 +8,6 @@ import pandas_profiling as pdp    return    return pdp.ProfileReport(df)
 from IPython.display import display
 
 
-
 class Nekoma:
 
     def __init__(self):
@@ -16,12 +15,16 @@ class Nekoma:
         self.test_set = pd.Dataframe()
         self.target = pd.Dataframe()
         self.back_up = self.train_set.copy()
+        self.transformations = []
+        self.counter = 0
 
     def __init__(train_set, test_set, target):
         self.train_set = train_set
         self.test_set = test_set
         self.target = target
         self.back_up = self.train_set.copy()
+        self.transformations = []
+        self.counter = 0
 
     def __init__(df_or_file, target, test_ratio, seed=1, strat=False):
         if type(df_or_file) == "str":
@@ -45,10 +48,13 @@ class Nekoma:
                     df_or_file, target, test_ratio, seed)
         self.target = target
         self.back_up = self.train_set.copy()
+        self.transformations = []
 
     def reset(self):
         self.train_set = pd.Dataframe()
         self.train_set = self.back_up.copy()
+        tr_counter = 0
+        self.transformations = []
 
     def setTrainSet(self, train_set):
         self.train_set = train_set
@@ -61,7 +67,7 @@ class Nekoma:
         self.target = target
 
     def info(self):
-        dict_df = {"Feature":[], "Non NA":[], "Type":[]}
+        dict_df = {"Feature": [], "Non NA": [], "Type": []}
         for line in self.train_set.split("\n")[3:-3]:
             dict_df["Feature"].append(line.split("    ")[0])
             dict_df["Non NA"].append(line.split("    ")[1].split(" ")[0])
@@ -92,4 +98,32 @@ class Nekoma:
             return pdp.ProfileReport(df)
         except:
             print("Error: can't show report")
+
+    def apply(self, _transformation_f, parameters):
+        tr_counter += 1
+        self.transformations.append((str(get_counter()) +
+                                    ". " + type(transformation).__name__,
+                                    transformation))
+        _transformation_f(self.)
+
+    def use_pipeline(self, df, name, update=True):
+        """
+        Use a pipeline by giving the associated name on a dataframe.
+
+        Parameters
+        ----------
+            df
+                type : Dataframe
+            name
+                type : string
+
+        """
+        for pipeline in self.tr_transformation_pipelines_collection:
+            if pipeline[0] == name:
+                pipeline[1].fit_transform(df)
+                if update:
+                    for transformation in pipeline[1].steps:
+                        self.add_transformation(transformation[1])
+                break
+
 
